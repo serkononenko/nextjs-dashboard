7c716a28aa4f9ace2fe34450406d8c8204cd31de
Serhii Kononenko <serhii.kononenko@pitchbook.com>
2024-01-19 11:02:38 +0200

    add images and fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import {clsx} from 'clsx';
+
+import {inter} from '@/app/ui/fonts';
+
+import '@/app/ui/global.css';
+
+import type {FC, ReactNode} from 'react';
+
+
+type Props = {
+    children: ReactNode;
 }
+
+const RootLayout: FC<Props> = ({children}) => (
+    <html lang="en">
+    <body className={clsx(inter.className, 'antialiased')}>{children}</body>
+    </html>
+)
+
+export default RootLayout;
