d211c0e9c76457e5fedf78859d372723d21b4a64
Serhii Kononenko <serhii.kononenko@pitchbook.com>
2024-01-19 16:48:13 +0200

    add pages

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import SideNav from '@/app/ui/dashboard/sidenav';
+
+import type {FC, ReactNode} from 'react';
+
+
+type Props = {
+    children: ReactNode
+}
+
+const Layout: FC<Props> = ({children}) => (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        <div className="w-full flex-none md:w-64">
+            <SideNav/>
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+)
+
+export default Layout;
